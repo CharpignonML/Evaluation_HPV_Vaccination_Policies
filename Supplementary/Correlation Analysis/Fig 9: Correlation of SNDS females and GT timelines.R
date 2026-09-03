72b213e19d97e917d447934f68833bf89c4e7c82
charlottederogis-sudo <charlotte.derogis@gmail.com>
2026-09-03 17:12:44 +0200

    Update and rename Fig 9: Correlation of SNDS and GT timelines.R to Fig 9: Correlation of SNDS females and GT timelines.R

--- Supplementary/Correlation Analysis/Fig 9: Correlation of SNDS females and GT timelines.R
+++ Supplementary/Correlation Analysis/Fig 9: Correlation of SNDS females and GT timelines.R
@@ -2,33 +2,27 @@ library(dplyr)
 library(tidyverse)
 library(ggplot2)
 
-## Donées SNDS
+## Donées SNDS - filles
 
-df_snds <- read.csv2("/Users/charlottederogis/Desktop/Stage/CSV\ populations/table_snds_total.csv")
+df_f <- read.csv("/Users/charlottederogis/Desktop/Stage/données\ SNDS/table_snds_filles.csv")
 
-df_snds$n <- as.numeric(df_snds$n)
-
-df_snds <- df_snds %>%   
+df_f <- df_f %>%   
   mutate(total_norm = ((n - min(n)) / (max(n) - min(n)))*100)
-
-df_snds$Date <- as.Date(df_snds$Date)
-  
-df_snds <- df_snds %>% 
+df_f$Date <- as.Date(df_f$Date)
+df_f <- df_f %>% 
   dplyr::select(Date, total_norm)
 
-
 ## Données google trends
-
 df_ggtrends <- read.csv2("/Users/charlottederogis/Desktop/Stage/R/CSV/google\ trends/ggt\ total\ dates\ snds.csv")
 df_ggtrends$Date <- as.Date(paste0(df_ggtrends$Date, "-01"), format = "%Y-%m-%d")
 
+
 # visuel 
 df_plot <- inner_join(
-  df_snds %>% select(Date, snds = total_norm),
+  df_f %>% select(Date, snds_f = total_norm),
   df_ggtrends %>% select(Date, ggtrends = n),
   by = "Date"
 )
-
 df_long <- df_plot %>% 
   pivot_longer(
     cols = -Date,
@@ -37,12 +31,12 @@ df_long <- df_plot %>%
   ) %>%
   mutate(Series = recode(Series,
                          "ggtrends" = "Google Trends (search intensity)",
-                         "snds" = "SNDS (pharmacy dispensations)"
+                         "snds_f" = "SNDS (pharmacy dispensations) - female"
   ))
 
 
 # Start with a usual ggplot2 call:
-plot_cor <- ggplot(df_long, aes(x = Date, y = n, color = Series)) +
+plot_f <- ggplot(df_long, aes(x = Date, y = n, color = Series)) +
   
   geom_line(linewidth = 0.6) +
   
@@ -50,7 +44,7 @@ plot_cor <- ggplot(df_long, aes(x = Date, y = n, color = Series)) +
     name = "Series",
     values = c(
       "Google Trends (search intensity)" = "#C75915",
-      "SNDS (pharmacy dispensations)" = "#800080"
+      "SNDS (pharmacy dispensations) - female" = "#800080"
     )
   ) +
   scale_y_continuous(
@@ -73,7 +67,7 @@ plot_cor <- ggplot(df_long, aes(x = Date, y = n, color = Series)) +
     legend.position = "top"
     #plot.margin = margin(t = 60, r = 10, b = 10, l = 10)
   )
-plot_cor
+plot_f
 
 save_plot <- function(plot, filename, width = 10, height = 6, dpi = 300) {
   ggsave(
@@ -87,21 +81,19 @@ save_plot <- function(plot, filename, width = 10, height = 6, dpi = 300) {
 }
 
 save_plot(
-  plot_cor,
-  "/Users/charlottederogis/Desktop/Stage/VF analyses/validés pour de vrai/correlation SNDS GT.png"
+  plot_f,
+  "/Users/charlottederogis/Desktop/Stage/VF analyses/validés pour de vrai/correlation SNDS - filles GT.png"
 )
-
-
 # transformation en time series
-ts_gt <- ts(df_ggtrends$n, start = c(2007, 07), frequency = 12)
-ts_snds <- ts(df_snds$total_norm, start = c(2007, 07), frequency = 12)
+ts_gt <- ts(df_ggtrends$n, start = c(2007, 07), frequency = 12)  # ou frequency = 1 si annuel
+ts_snds_f <- ts(df_f$total_norm, start = c(2007, 07), frequency = 12)
 
 # même dates
-gt <- ts.intersect(ts_gt, ts_snds)[, "ts_gt"]
-snds <- ts.intersect(ts_gt, ts_snds)[, "ts_snds"]
+gt <- ts.intersect(ts_gt, ts_snds_f)[, "ts_gt"]
+snds_f <- ts.intersect(ts_gt, ts_snds_f)[, "ts_snds_f"]
 
 # corrélation de Pearson
-cor.test(gt, snds, method = "pearson")
+cor.test(gt, snds_f, method = "pearson")
 
 # corrélation de spearman
-cor.test(gt, snds, method = "spearman")
+cor.test(gt, snds_f, method = "spearman")
